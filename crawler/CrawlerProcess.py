d2a485729ee8e3effe71c2a4908280ed55139115
Sreedhar Bukya <sreedhar@kisanhub.com>
2015-07-05 10:43:11 +0530

    SB:added some more test cases for crawler process

--- crawler/CrawlerProcess.py
+++ crawler/CrawlerProcess.py
@@ -1,14 +1,12 @@
-import pprint
 import urllib2
 import urlparse
-from urlparse import urlparse as url_parser
 import time
 
 from BeautifulSoup import BeautifulSoup
 
 
 class CrawlerProcess():
-    def __init__(self, start_url, maximum_links=11):
+    def __init__(self, start_url, maximum_links=30):
         self.start_url = start_url
         self.maximum_links = maximum_links
 
@@ -25,23 +23,19 @@ class CrawlerProcess():
             time.sleep(2)
 
     def process_html_page_and_set_status_of_crawling(self, current_url):
-        status = CrawlerProcess.check_url_status(current_url)
-        if status:
-            try:
-                html_dump = self.url_handler(current_url)
-                soup = BeautifulSoup(html_dump)
-                for link in soup.findAll('a'):
-                    found_url = urlparse.urljoin(current_url, link.get('href'))
-                    if self.counter < self.maximum_links:
-                        if found_url.startswith(current_url):
-                            if found_url not in self.status_dict:
-                                self.status_dict[found_url] = False
-                                self.counter += 1
-                self.status_dict[current_url] = True
-            except:
-                self.status_dict[current_url] = True
-        else:
-            print "Problem found in validating URL: %s" % current_url
+        try:
+            html_dump = self.url_handler(current_url)
+            soup = BeautifulSoup(html_dump)
+            for link in soup.findAll('a'):
+                found_url = urlparse.urljoin(current_url, link.get('href'))
+                if self.counter < self.maximum_links:
+                    if found_url.startswith(current_url):
+                        if found_url not in self.status_dict:
+                            self.status_dict[found_url] = False
+                            self.counter += 1
+            self.status_dict[current_url] = True
+        except:
+            self.status_dict[current_url] = True
 
     def more_to_crawl_check(self):
         for url, status in iter(self.status_dict.iteritems()):
@@ -78,17 +72,8 @@ class CrawlerProcess():
             print 'Problem in Response for URL %s with Exception %s' % url, ex
             return None
 
-    @staticmethod
-    def check_url_status(url):
-        status = False
-        parse = url_parser(url)
-        if parse.netloc or parse.path:
-            status = True
-        return status
-
 
 if __name__ == "__main__":
     inputURL = "https://www.python.org/"
     handler = CrawlerProcess(inputURL)
     handler.start_web_crawler()
-    pprint.pprint(handler.status_dict)
--- tests/CrawlerProcessTestCase.py
+++ tests/CrawlerProcessTestCase.py
@@ -5,10 +5,10 @@ from crawler.CrawlerProcess import CrawlerProcess
 class CrawlerProcessTestCase(TestCase):
     def setUp(self):
         self.first_url = 'www.google.co.in'
-        self.crawler = CrawlerProcess(self.first_url)
+        self.crawler = CrawlerProcess(self.first_url, 30)
 
         self.second_url = "https://www.python.org/"
-        self.second_crawler = CrawlerProcess(self.second_url)
+        self.second_crawler = CrawlerProcess(self.second_url, 2)
 
     def test_for_Valid_url_string(self):
         expected_url = self.first_url
@@ -20,12 +20,26 @@ class CrawlerProcessTestCase(TestCase):
         actual_url = self.crawler.start_url
         self.assertNotEquals(actual_url, expected_url)
 
-    def test_for_valid_response(self):
+    def test_for_html_process_status_dict(self):
         expected_output = True
-        actual_output = CrawlerProcess.check_url_status(self.first_url)
-        self.assertEqual(expected_output, actual_output)
+        self.crawler.start_web_crawler()
+        actual_output = self.crawler.status_dict.get(self.first_url)
+        self.assertEqual(actual_output, expected_output)
 
-    def test_for_invalid_response(self):
-        expected_output = False
-        actual_output = CrawlerProcess.check_url_status('google.com')
+    def test_for_valid_maximum_links(self):
+        expected_output = 2
+        self.second_crawler.start_web_crawler()
+        actual_output = len(self.second_crawler.status_dict.keys())
+        self.assertEqual(actual_output, expected_output)
+
+    def test_for_invalid_maximum_links(self):
+        expected_output = 10
+        self.crawler.start_web_crawler()
+        actual_output = len(self.crawler.status_dict.keys())
         self.assertNotEquals(actual_output, expected_output)
+
+    def test_for_valid_less_than_maximum_links(self):
+        expected_output = 40
+        self.crawler.start_web_crawler()
+        actual_output = len(self.crawler.status_dict.keys())
+        self.assertLess(actual_output, expected_output)
